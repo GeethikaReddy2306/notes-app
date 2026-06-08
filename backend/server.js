2a549b4cf3db7a726056c0f060a5811a31a64c5a
geethika <geethikareddy051@gmail.com>
2026-06-08 15:39:15 +0530

    feat: enhance server configuration for production and development environments

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,31 @@ const express=require('express');
 const app=express();
 const dns=require('dns');
 const cors=require('cors');
-//middleware
+const path=require('paths');
+
+const __dirname=path.resolve()
 app.use(express.json());
 app.use(cors({
         origin: process.env.Frontend
 }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 dns.setServers(['1.1.1.1','8.8.8.8']);
 const connectDb=require('./src/config/db');
 const router=require('./src/routes/notes.routes');
 app.use('/api/notes',router);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT,()=>{
         console.log("Server us running successfully");
