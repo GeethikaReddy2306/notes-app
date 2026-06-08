878b5aa9b2dfb20ae7721dc87cb307060f7753c4
geethika <geethikareddy051@gmail.com>
2026-06-08 16:00:47 +0530

    refactor: reorganize server.js for improved readability and maintainability

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,48 @@
-const express=require('express');
-const app=express();
-const dns=require('dns');
-const cors=require('cors');
-const path=require('path');
-require('dotenv').config();
-const __dirname=path.resolve()
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const dns = require("dns");
+const cors = require("cors");
+const path = require("path");
+
+const connectDb = require("./src/config/db");
+const router = require("./src/routes/notes.routes");
+
+
+
+// Middleware
 app.use(express.json());
-app.use(cors({
-        origin: process.env.Frontend
-}));
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-    })
-  );
-}
-dns.setServers(['1.1.1.1','8.8.8.8']);
-const connectDb=require('./src/config/db');
-const router=require('./src/routes/notes.routes');
-app.use('/api/notes',router);
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.Frontend
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+// Routes
+app.use("/api/notes", router);
+
+// Serve React frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend", "dist", "index.html")
+    );
   });
 }
 
-app.listen(process.env.PORT,()=>{
-        console.log("Server us running successfully");
-})
+// Start server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
